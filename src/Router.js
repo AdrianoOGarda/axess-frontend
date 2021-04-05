73126ac62d73bf99abf387da6a045380ca992630
Adriano Ortiz Garda <adriano.ortizgarda@gmail.com>
2021-04-05 15:16:30 -0500

    tourConfig creada

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import {Home, Login, Signup, Beds, Cart, Nightstands, CreateFurniture, EditFurniture} from "./pages"
+import {Home, Login, Signup, Beds, Cart, Nightstands, CreateFurniture, EditFurniture, TourConfig} from "./pages"
 
 import NotFound from './components/404/NotFound.js';
 import Layout from "./components/layout"
@@ -16,6 +16,7 @@ const Router = () => (
         <Route exact path="/nightstands" component={Nightstands}/>
         <Route exact path="/cart" component={Cart}/>
         <Route exact path="/create-furniture" component={CreateFurniture}/>
+        <Route exact path="/tour-select" component={TourConfig}/>
         <Route exact path="/:furnitureId" component={EditFurniture}/>
         <Route component={NotFound} />
       </Switch>
---
+++ src/pages/TourConfig.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../css/tourConfig.css"
+import "../css/typography.css"
+
+
+function TourConfig() {
+
+    return (
+        <div className="tour-config-main-div">
+
+            <div className="tour-config-title-div">
+                <div className="tour-config-divider"></div>
+                <h1>Recorrido guiado</h1>
+                <div className="tour-config-divider"></div>
+            </div>
+
+            <div>
+                <p>Selecciona el proyecto al que pertenece tu departamento</p>
+                
+            </div>
+
+        </div>
+    )
+}
+
+export default TourConfig
