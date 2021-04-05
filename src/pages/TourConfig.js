97d11b437be4b9473d4842a073189e41102192aa
Adriano Ortiz Garda <adriano.ortizgarda@gmail.com>
2021-04-05 16:10:48 -0500

    Avance TourConfig

--- src/pages/TourConfig.js
+++ src/pages/TourConfig.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Select, InputNumber } from 'antd';
 import "../css/tourConfig.css"
 import "../css/typography.css"
 
+const { Option } = Select;
+
 
 function TourConfig() {
+
+    const [bedrooms, setBedrooms] = useState(null);
+
+    function handleChangeProject(value) {
+        console.log(`selected ${value}`);
+    }
+
+    function onChangeBedrooms(value) {
+        setBedrooms(value);
+    }
 
     return (
         <div className="tour-config-main-div">
@@ -16,7 +29,19 @@ function TourConfig() {
 
             <div>
                 <p>Selecciona el proyecto al que pertenece tu departamento</p>
-                
+                <Select defaultValue="AWA" onChange={handleChangeProject} className='tour-config-project-select'>
+                    <Option value="AWA">AWA</Option>
+                </Select>
+            </div>
+
+            <div>
+                <p>¿Cuántas recámaras tiene tu departamento?</p>
+                <InputNumber min={1} max={3} onChange={onChangeBedrooms} className='tour-config-bedrooms-select' />
+            </div>
+
+            <div>
+                <button>Cancelar</button>
+                <button>Continuar</button>
             </div>
 
         </div>
